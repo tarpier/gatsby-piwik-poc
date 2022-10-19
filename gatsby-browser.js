288ac1c26f68ce096d5e35ddaefbf00061eb2c5c
Ove Doll <ove@ovedoll.de>
2022-10-19 12:55:16 +0200

    fix(piwik): fixes double pagetracking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,22 +1,24 @@
 // custom typefaces
-require( "typeface-montserrat")
-require( "typeface-merriweather")
+require("typeface-montserrat")
+require("typeface-merriweather")
 // normalize CSS across browsers
-require( "./src/normalize.css")
+require("./src/normalize.css")
 // custom CSS styles
-require( "./src/style.css")
+require("./src/style.css")
 
 // Highlighting for code blocks
-require( "prismjs/themes/prism.css")
+require("prismjs/themes/prism.css")
 
 
 const PiwikPro = require("@piwikpro/react-piwik-pro").default;
 const { PageViews } = require("@piwikpro/react-piwik-pro");
 
-exports.onClientEntry = (_, pluginOptions) => {
-        PiwikPro.initialize('1cbc6598-083c-42f5-8798-a95d58c2a53b', 'https://bemergroup.piwik.pro');
+exports.onClientEntry = () => {
+    console.log('initialize')
+    PiwikPro.initialize('1cbc6598-083c-42f5-8798-a95d58c2a53b', 'https://bemergroup.piwik.pro');
 };
 
 exports.onRouteUpdate = ({ location, prevLocation }) => {
+    console.log('pageview', location)
     PageViews.trackPageView(location);
 };
